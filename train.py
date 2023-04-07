2ad5c4abb4d19aaa06e18648f4b0bf1957c96e0e
thomashopkins32 <thomashopkins000@gmail.com>
2023-04-07 14:12:36 -0400

    Functionality testing

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
+import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+import torchvision
 from tqdm import tqdm
 
 from dataset import Chars74k
@@ -10,10 +12,10 @@ from models import BaselineCNN
 from utils import accuracy
 
 # PARAMETERS
-BATCH_SIZE = 64
-LR = 0.01
-WD = 1e-4
-MOMENTUM = 0.9
+BATCH_SIZE = 32
+LR = 0.1
+WD = 0.0
+MOMENTUM = 0.0
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 VALID_STEP = 10
 RNG = 2
@@ -26,8 +28,19 @@ train_data = Chars74k(test=False)
 train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 model = BaselineCNN().to(DEVICE)
 loss_func = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=LR)
+optimizer = optim.SGD(model.parameters(), lr=LR)
 global_step = 0
+
+'''
+images, labels = next(iter(train_loader))
+x = images.to(DEVICE)
+y = labels.long().to(DEVICE)
+logits = model(x)
+loss = loss_func(logits, y)
+grid = torchvision.utils.make_grid(images)
+writer.add_image('images', grid, 0)
+writer.add_graph(model, images)
+'''
 
 for e in tqdm(range(EPOCHS)):
     for i, (x, y) in enumerate(train_loader):
@@ -41,7 +54,7 @@ for e in tqdm(range(EPOCHS)):
         optimizer.step()
 
         if i % VALID_STEP == 0:
-            global_step += i
+            global_step += 1
             writer.add_scalar("Loss/train", loss.item(), global_step)
             writer.add_scalar("Accuracy/train", accuracy(logits, y), global_step)
 writer.close()

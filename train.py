052ecc9bea78eb9be7a1efa1df3eda6ac39a304a
Thomas Hopkins <thomashopkins000@gmail.com>
2023-04-15 12:09:23 -0400

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 import torchvision
 from tqdm import tqdm
@@ -12,8 +12,8 @@ from models import BaselineCNN
 from utils import accuracy
 
 # PARAMETERS
-BATCH_SIZE = 32
-LR = 0.1
+BATCH_SIZE = 128
+LR = 3e-4
 WD = 0.0
 MOMENTUM = 0.0
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -24,11 +24,14 @@ EPOCHS = 100
 torch.manual_seed(RNG)
 
 writer = SummaryWriter()
-train_data = Chars74k(test=False)
+dataset = Chars74k(test=False)
+generator = torch.Generator().manual_seed(RNG)
+train_data, valid_data = random_split(dataset, [0.9, 0.1], generator=generator)
 train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+valid_loader = DataLoader(valid_data, batch_size=len(valid_data), shuffle=False)
 model = BaselineCNN().to(DEVICE)
 loss_func = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=LR)
+optimizer = optim.Adam(model.parameters(), lr=LR)
 global_step = 0
 
 '''
@@ -57,4 +60,11 @@ for e in tqdm(range(EPOCHS)):
             global_step += 1
             writer.add_scalar("Loss/train", loss.item(), global_step)
             writer.add_scalar("Accuracy/train", accuracy(logits, y), global_step)
+            for i, (x, y) in enumerate(valid_loader):
+                x = x.to(DEVICE)
+                y = y.to(DEVICE)
+                logits = model(x)
+                loss = loss_func(logits, y)
+                writer.add_scalar("Loss/valid", loss.item(), global_step)
+                writer.add_scalar("Accuracy/valid", accuracy(logits, y), global_step)
 writer.close()

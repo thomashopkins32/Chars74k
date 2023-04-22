108bf19f0243cd0b1602f9ee31c3bda5ba1d0ee6
Thomas Hopkins <thomashopkins000@gmail.com>
2023-04-22 15:01:25 -0400

    New model

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 # Dataset specific configurations
 NUM_CLASSES = 62
 
@@ -32,6 +33,39 @@ class BaselineCNN(nn.Module):
         x = self.bn2(x)
         x = F.relu(x)
         x = self.conv3(x)
+        x = self.avg_pool(x)
+        x = F.relu(x)
+        x = self.flatten(x)
+        x = self.linear(x)
+        return x
+
+
+class CNNv2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5, padding=2, bias=False)
+        self.maxp1 = nn.MaxPool2d(2, stride=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1, bias=False)
+        self.maxp2 = nn.MaxPool2d(2, stride=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1, bias=False)
+        self.maxp3 = nn.MaxPool2d(2, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1, bias=False)
+        self.maxp4 = nn.MaxPool2d(2, stride=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 256, 3)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear(256, NUM_CLASSES)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.maxp1(self.conv1(x))))
+        x = F.relu(self.bn2(self.maxp2(self.conv2(x))))
+        x = F.relu(self.bn3(self.maxp3(self.conv3(x))))
+        x = F.relu(self.bn4(self.maxp4(self.conv4(x))))
+        x = self.conv5(x)
         x = self.avg_pool(x)
         x = F.relu(x)
         x = self.flatten(x)
--- train.py
+++ train.py
@@ -7,19 +7,22 @@ from torch.utils.tensorboard import SummaryWriter
 import torchvision
 from tqdm import tqdm
 
-from dataset import Chars74k
-from models import BaselineCNN
-from utils import accuracy
+from dataset import *
+from models import *
+from utils import *
 
 # PARAMETERS
-BATCH_SIZE = 128
-LR = 3e-4
-WD = 0.0
-MOMENTUM = 0.0
+BATCH_SIZE = 64
+LR = 0.1
+WD = 1e-8
+MOMENTUM = 0.9
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 VALID_STEP = 10
-RNG = 2
-EPOCHS = 100
+RNG = 32
+EPOCHS = 300
+
+SHOW_MODEL = True
+SHOW_SAMPLES = False
 
 torch.manual_seed(RNG)
 
@@ -29,21 +32,24 @@ generator = torch.Generator().manual_seed(RNG)
 train_data, valid_data = random_split(dataset, [0.9, 0.1], generator=generator)
 train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 valid_loader = DataLoader(valid_data, batch_size=len(valid_data), shuffle=False)
-model = BaselineCNN().to(DEVICE)
+model = CNNv2().to(DEVICE)
 loss_func = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=LR)
+optimizer = optim.SGD(model.parameters(), lr=LR, weight_decay=WD, momentum=MOMENTUM)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [20, 100, 200], 0.1, verbose=False)
 global_step = 0
 
-'''
-images, labels = next(iter(train_loader))
-x = images.to(DEVICE)
-y = labels.long().to(DEVICE)
-logits = model(x)
-loss = loss_func(logits, y)
-grid = torchvision.utils.make_grid(images)
-writer.add_image('images', grid, 0)
-writer.add_graph(model, images)
-'''
+if SHOW_MODEL:
+    images, labels = next(iter(train_loader))
+    x = images.to(DEVICE)
+    writer.add_graph(model, x)
+if SHOW_SAMPLES:
+    images, labels = next(iter(train_loader))
+    x = images.to(DEVICE)
+    y = labels.long().to(DEVICE)
+    logits = model(x)
+    loss = loss_func(logits, y)
+    grid = torchvision.utils.make_grid(images)
+    writer.add_image('images', grid, 0)
 
 for e in tqdm(range(EPOCHS)):
     for i, (x, y) in enumerate(train_loader):
@@ -67,4 +73,5 @@ for e in tqdm(range(EPOCHS)):
                 loss = loss_func(logits, y)
                 writer.add_scalar("Loss/valid", loss.item(), global_step)
                 writer.add_scalar("Accuracy/valid", accuracy(logits, y), global_step)
+    scheduler.step()
 writer.close()

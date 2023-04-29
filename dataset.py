0ad2e817e0637f1905d0adf53850bd2b8f692777
Thomas Hopkins <thomashopkins000@gmail.com>
2023-04-29 16:39:52 -0400

    Try some data transformations

--- dataset.py
+++ dataset.py
@@ -1,6 +1,5 @@
 import os
 
-import matplotlib.pyplot as plt
 import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
@@ -15,19 +14,27 @@ class Chars74k(Dataset):
         else:
             self.path = 'trainResized/'
         self.labels = pd.read_csv('trainLabels.csv', index_col='ID')
-        self.transform = tv.transforms.ToTensor()
+        self.train_transform = tv.transforms.Compose(
+            [
+                tv.transforms.ColorJitter(brightness=0.5, hue=0.3),
+                tv.transforms.ToTensor(), 
+            ]
+        )
+        self.test_transform = tv.transforms.ToTensor()
         self.idx_to_char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
         self.char_to_idx = {c: i for i, c in enumerate(self.idx_to_char)}
+        self.images = []
+        for i in range(1, len(self.labels) + 1):
+            self.images.append(Image.open(os.path.join(self.path, f'{i}.Bmp')).convert('RGB'))
 
     def __getitem__(self, i):
+        image = self.images[i]
         if self.test:
-            i += 6285 # offset for file name
+            tensor = self.test_transform(image)
             y = None
         else:
-            i += 1
-            y = self.char_to_idx[self.labels.loc[i].item()]
-        image = Image.open(os.path.join(self.path, f'{i}.Bmp')).convert('RGB')
-        tensor = self.transform(image) 
+            tensor = self.train_transform(image) 
+            y = self.char_to_idx[self.labels.loc[i + 1].item()]
         assert tensor.shape == (3, 20, 20)
         return tensor, y
 
